165d76256d82b20cac0a199da231be11bd807b96
oheneemmanuel <ohenekwakye24@gmail.com>
2025-09-03 12:15:54 +0000

    added layout, partial to the views

--- server.js
+++ server.js
@@ -6,14 +6,31 @@
  * Require Statements
  *************************/
 const express = require("express")
+const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
 
+
+/* ***********************
+ * View Engine and template
+ *************************/
+
+
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout") // not at view root 
+/*app.use(static)*/
 /* ***********************
  * Routes
  *************************/
 app.use(static)
+
+
+// index route 
+app.get("/", function(req, res) {
+  res.render("index", {title: "Home"})
+})
 
 /* ***********************
  * Local Server Information
